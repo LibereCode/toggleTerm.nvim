ca34b4542d3127aced565f52259774e0293b4548
Libere Code <libere-code@posteo.org>
2026-05-05 18:56:59 +0200

    after clean
    removed old functions

--- lua/toggleTerm.lua
+++ lua/toggleTerm.lua
@@ -21,7 +21,7 @@ M.state = {
 ---@field border? string -- bordertype
 
 ---@param opts toggle_float_opts
---- merging `M.toggle()` and `M.float()` --PERF: WORKS
+--- merging `M.toggle()` and `M.float()`
 M.toggle_float = function(opts)
     if vim.api.nvim_win_is_valid(M.state.floating.win) then -- if visible
         vim.api.nvim_win_hide(M.state.floating.win) -- hide
@@ -61,7 +61,7 @@ end
 ---@field win? integer -- -1(default) = across all windows OR 0 = just current
 
 ---@param opts? toggle_hor_opts
---- merging `M.toggle()` and `M.float()` --PERF: WORKS
+--- merging `M.toggle()` and `M.horizonstal()`
 M.toggle_hor = function(opts)
     if vim.api.nvim_win_is_valid(M.state.floating.win) then -- if visible
         vim.api.nvim_win_hide(M.state.floating.win) -- then hide
@@ -93,103 +93,10 @@ M.toggle_hor = function(opts)
     end
 end
 
--- FIXME: REMOVE THESE OLD FUNCTIONS BELOW !!
-
----@class bufwinReturn
----@field buf integer
----@field win integer
-
----@class openWinOpts
----@field x? number -- %width,  between 0.0 and 1.0
----@field y? number -- %height,  between 0.0 and 1.0
----@field border? string -- bordertype
-
----@param opts openWinOpts
----@return bufwinReturn
-function M.float(opts)
-    opts = opts or {}
-    local x = opts.x or 0.9
-    local y = opts.y or 0.9
-
-    -- TODO: Understand (and fix?) this part
-    local buf
-    if vim.api.nvim_buf_is_valid(M.state.floating.buf) then
-        buf = M.state.floating.buf
-    else
-        buf = vim.api.nvim_create_buf(false, true) -- buffer be like
-    end
-
-    local win_conf = {
-        relative = "editor",
-        width = math.floor(vim.o.columns * x),
-        height = math.floor(vim.o.lines * y),
-        col = math.floor(vim.o.columns * ((1 - x) / 2)),
-        row = math.floor(vim.o.lines * ((1 - y) / 2)),
-        border = opts.border or "double",
-    }
-    local win = vim.api.nvim_open_win(buf, true, win_conf)
-
-    return { buf = buf, win = win }
-end
-
----@class horOpts
----@field y? number 0.0<x<=1.0
-
----@param opts horOpts
----@return bufwinReturn
-function M.hor(opts)
-    opts = opts or {}
-
-    local buf
-    if vim.api.nvim_buf_is_valid(M.state.floating.buf) then
-        buf = M.state.floating.buf
-    else
-        buf = vim.api.nvim_create_buf(false, true) -- buffer be like
-    end
-
-    local win_conf = {
-        height = math.floor(vim.o.lines * (opts.y or 0.2)), -- FIXME:? or 0.2 ?
-    }
-    local win = vim.api.nvim_open_win(buf, true, win_conf)
-
-    return { buf = buf, win = win }
-end
-
----@param termType? string -- TODO: REMOVE/CHANGE (read below)
----@param opts? openWinOpts
-function M.toggle(termType, opts)
-    if vim.api.nvim_win_is_valid(M.state.floating.win) then -- if visible
-        vim.api.nvim_win_hide(M.state.floating.win) -- hide
-    else
-        local y, x, border = 0.8, 0.8, "rounded"
-        if opts then -- kind of cursed:
-            y, x, border = opts.y or y, opts.x or x, opts.border or border
-        end
-        if termType == "hor" then
-            M.state.floating = M.hor({
-                y = y,
-            }) -- tells it to use the same buffer
-            if vim.bo[M.state.floating.buf].buftype ~= "terminal" then -- if buftype isn't terminal
-                vim.cmd.terminal() -- enter terminal
-            end
-        -- end
-        else -- if termType == "float" then --NOTE: WIP
-            M.state.floating = M.float({
-                x = x,
-                y = y,
-                border = border,
-            }) -- tells it to use the same buffer
-            if vim.bo[M.state.floating.buf].buftype ~= "terminal" then -- if buftype isn't terminal
-                vim.cmd.terminal() -- enter terminal
-            end
-        end
-    end
-    vim.cmd("startinsert")
-end
-
+--DONE:
+--1. [x] toggle_float()
+--2. [x] toggle_horizontal()
 --TODO: MERGE MERGE(/split) FUNCTIONS INTO:
---1. toggle_float()
---2. toggle_horizontal()
 --Then add a third
 --3. toggle_vertical()
 
@@ -203,7 +110,6 @@ end
 return M
 
 -- TODO: ideas of other options:
---
 -- map('n', '<leader>tv', function() require('custom.toggle_term').verti() end, { desc = 'Vertical terminal' })
 -- map('n', '<leader>th', function() require('custom.toggle_term').horiz() end, { desc = 'Horizontal terminal' })
 -- map({ 'n', 't' }, '<M-t>', function() require('custom.toggle_term').float() end, { desc = 'Toggle Term' })
